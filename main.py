b82e8f7e43930d6605a8bd7eb410983ec126a39e
douglas-ods <douglas_ods@yahoo.com.br>
2023-03-14 06:16:42 -0300

    Adicionando função de coleta de dados

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def gether_data():
+    n1 = int(input("Primeiro valor: "))
+    n2 = int(input("Segundo valor: "))
+    return n1, n2
 def main():
-    print(2*2)
+    n1, n2 = gether_data()
+    print(n1*n2)
     return None
+if __name__ =="__main__":
+    main()
