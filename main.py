0bf9e88e98dd1e68685fc0bae2d09af4baa533c6
douglas-ods <douglas_ods@yahoo.com.br>
2023-03-14 06:21:49 -0300

    Adicionado operação

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def gether_data():
     n1 = int(input("Primeiro valor: "))
     n2 = int(input("Segundo valor: "))
-    return n1, n2
+    op = input("Operação:")
+    return n1, n2,op
 def main():
-    n1, n2 = gether_data()
+    n1, n2,op = gether_data()
     print(n1**n2)
     return None
 if __name__ =="__main__":
